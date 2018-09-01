6a0844eb8c1356c7d7efb9f3b05946a8fa3eb09a
vishwatej reddy <reddyvishwatej176@gmail.com>
2018-09-01 00:32:22 -0400

    Implemented the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { ButtonComponent } from './button/button.component';
+import { ProjectComponent } from './project/project.component';
+import { ServicesComponent } from './services/services.component';
+import { CertificateComponent } from './certificate/certificate.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        NavigationComponent,
+        AboutComponent,
+        ButtonComponent,
+        ProjectComponent,
+        ServicesComponent,
+        CertificateComponent,
+        ContactComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
